66b80e8c64b7d86b771ab4163e6dd72e07cc6a49
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2024-08-28 19:08:13 +0700

    add code first time

---
+++ src/main/java/com/spring/entities/Customer.java
@@ -0,0 +1,50 @@
+package com.spring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Date_Of_Birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "National_ID_No", nullable = false)
+    private String nationalIdNo;
+
+    @Column(name = "Phone_No", nullable = false)
+    private String phoneNo;
+
+    @Column(name = "Email", nullable = false)
+    private String email;
+
+    @Column(name = "Address", nullable = false)
+    private String address;
+
+    @Column(name = "Driving_License", nullable = false)
+    private String drivingLicense;
+
+    @Column(name = "Wallet", precision = 18, scale = 2, nullable = false)
+    private BigDecimal wallet;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
+    private List<Booking> bookings = new ArrayList<>();
+}

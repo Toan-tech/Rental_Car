61dbd6088835bf848aca15e6c0a8dfe2bc3b4b09
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2024-08-28 19:26:35 +0700

    Delete src directory

--- src/main/java/com/spring/entities/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.spring.entities;
-
-public enum PaymentMethod {
-    My_Wallet,
-    Cash,
-    Bank_Transfer
-}
--- src/main/java/com/spring/entities/RoleEnum.java
+++
@@ -1,7 +0,0 @@
-package com.spring.entities;
-
-public enum RoleEnum {
-    Car_Owner,
-    Customer,
-    Guest
-}

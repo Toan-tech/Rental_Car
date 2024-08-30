c22fe4367bd351e811cc81ee29ac276704b6d670
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2024-08-30 20:14:27 +0700

    Update Work

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping(value ={ "/"})
     public String home(Model model) {
-        return "layout/Homepage";
+        return "layout/Thumbview";
     }
 }
